fcd1658ec390766d10bec51c19e047c4220ee7e7
n00bit <n00bit32@outlook.com>
2020-04-28 16:10:34 -0500

    example jenkins

---
+++ src/main/java/com/spring/jenkins/app/TestingJenkinsAppApplication.java
@@ -0,0 +1,13 @@
+package com.spring.jenkins.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingJenkinsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingJenkinsAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/jenkins/app/TestingJenkinsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.jenkins.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingJenkinsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
